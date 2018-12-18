154c78acdcadb18c7b9ca1486f528db9c0cf342f
unknown <jaepyojung@koreanair.com>
2018-12-18 13:31:36 +0900

    adding js

--- doc/01_memberdbtest_for_shell.js
+++ doc/01_memberdbtest_for_shell.js
@@ -1,4 +1,7 @@
+import { get } from "http";
 
+db
+use
 // 현재 DB 삭제
 db.runCommand({dropDatabase: 1});
 
@@ -9,32 +12,45 @@ var m3 = {name: "admin", age: 35};
 
 // TODO 1. member 컬렉션에 데이터 등록
 // insert({등록할 문서})
-
+db.member.insert(m1);   //하나만 insert할떄
+db.member.insert([m2,m3]); //여러개 insert할때 는 배열로 []
 
 
 // TODO 2. member 컬렉션 조회
 // find()
+db.member.find();
 
 
 // TODO 3. 회원 조회(나이가 20인 회원 조회)
 // find({검색조건})
+db.member.find({age : 20});
+db.member.find({ name : "kim"});
+db.member.find({ name : "kim" , age : 20 });
 
+db.member.find({age: {$gte : 20}});
+// $gt $gte $lt $lte
 
 // TODO 4. 회원 조회(1건)
 // findOne()
-
+db.member.findOne();  //rownum = 1 , id 값 작은걸로하여금 먼저나옴
+db.member.findOne({age:20});
 
 // TODO 5. 회원 수정(kim의 나이 수정)
 // update({검색조건}, {수정할 문서})
 // 지정한 문서 전체를 수정
+db.member.update({name:"kim"},{age:25}); //주의, kim을 찾아서 통째로교체 , find & delete & insert
+// { "_id" : ObjectId("5c1855799a1eef73f453b419"), "name" : "kim", "age" : 20 } 가
+// { "_id" : ObjectId("5c1855799a1eef73f453b419"), "age" : 25 } 이렇게 바껴버린다
 
 // 지정한 속성만 수정할 경우
+db.memeber.update({name : 'lee'},{$set:{age:25}});
 
 // 지정한 속성을 증가시킬 경우
-
+db.member.update({name:"lee"},{$inc:{age:1}});
 
 // TODO 6. lee 삭제
 // remove({검색 조건})
+db.member.remove({name:"lee"});
 
 
 
--- doc/02_boarddbtest_for_mongodriver.js
+++ doc/02_boarddbtest_for_mongodriver.js
@@ -23,12 +23,40 @@ switch(logLevel){
 */
 
 // DB 접속
+var conn;
+var db;
+
+
+// MongoClient.connect('mongodb://jaepyo:a12345@ds225624.mlab.com:25624',function(err,client){
+MongoClient.connect('mongodb://localhost:27017',function(err,client){
+  if(err){
+    clog.error(err);
+  }else{
+    clog.info('DB접속완료');
+    conn = client;
+    db =client.db('mpang'); //mydb 대신
+    db.command({dropDatabase: 1} , function(){
+      clog.info('DB 초기화완료.');
+      db.collection('board' , function(err,coll){
+        db.board = coll;
+        todo1();
+        setTimeout(function(){
+          client.close();
+        }, 1000 * 3);
+        
+
+      });
+    });
+  }
+});
+
+
 
 
 // 로그 메세지 출력
 function myLog(str, result){
 	clog.info(str);
-	clog.debug(util.inspect(result) + "\n");
+	clog.debug(util.inspect(result) + "\n"); //util core모듈에 inspect를사용하면배열을 json형태로 잘 뿌려준다
 }
 
 
@@ -43,48 +71,90 @@ var b2 = [
 // TODO 1. board 컬렉션에 데이터 등록
 // insert({등록할 문서})
 function todo1(){
-	
+  //case1 , 순차실행이 보장안됨
+  // db.board.insert(b1);
+  // db.board.insert(b2);
+  // todo2();
+
+  //case1 answer
+    // db.board.insert(b1, function(){
+    //   db.board.insert(b2, todo2);
+    // });
+
+  //case1 2answer
+    db.board.insert(b1,function(){
+      db.board.insert(b2,function(){
+        todo2();
+      })
+    });
 }
 
 // TODO 2. 모든 board 데이터의 모든 속성 조회
 // find()
 function todo2(){
-	
+  db.board.find().toArray(function(err, list ){
+    myLog('TODO 2. 모든 board 데이터의 모든 속성 조회',list);
+    todo3();
+  });	
+  
 }
 
 // TODO 3. 데이터 조회(kim이 작성한 게시물 조회)
 // find({검색조건})
 function todo3(){
-	
+  db.board.find({name:"kim"}).toArray(function(err, list){
+    myLog('// TODO 3. 데이터 조회(kim이 작성한 게시물 조회)',list);
+    todo4();
+  });	
 }
 
 // TODO 4. 모든 board 데이터의 작성자 속성만 조회(_id 포함)
 // find({검색조건}, {출력컬럼})
 function todo4(){
-	
+  db.board.find({},{name:1}).toArray(function(err, list){
+    myLog('TODO 4. 모든 board 데이터의 작성자 속성만 조회(_id 포함)',list);
+    todo5();
+  });	
 }
 
 // TODO 5. kim이 작성한 게시물의 제목 조회(_id 미포함)
 // find({검색조건}, {출력컬럼})
 function todo5(){
-	
+  db.board.find({},{name:1,_id:0}).toArray(function(err, list){
+    myLog('TODO 5. kim이 작성한 게시물의 제목 조회(_id 미포함)',list);
+    todo6();
+  });	
 }
 
 // TODO 6. 첫번째 게시물 조회(1건)
 // findOne()
 function todo6(){
-	
+  db.board.findOne(function(err,data){
+    myLog('TODO 6. 첫번째 게시물 조회(1건)',data);
+    todo7();
+  });	
 }
 
 // TODO 7. 게시물 조회(lee가 작성한 데이터 1건 조회)
 // findOne({검색조건})
 function todo7(){
-	
+  db.board.findOne({ name:"lee" } ,function(err,data){
+    myLog('TODO 7. 게시물 조회(lee가 작성한 데이터 1건 조회)',data);
+    todo8();
+  });	
+
+  // db.board.findOne({ name:"lee" },{name:1 , _id:0 } ,function(err,data){
+  //   myLog('TODO 7. 게시물 조회(lee가 작성한 데이터 1건 조회)',data);
+  //   todo8();
+  // });	
 }
 
 // TODO 8. 게시물 수정(3번 게시물의 내용 수정)
 // update({검색조건}, {수정할 문서})
 function todo8(){
+  db.board.update({no:3},{$set:{content: "수정."}}, function(){
+    list('TODO 8. 게시물 수정(3번 게시물의 내용 수정)',todo9);
+  });
 	
 }
 
